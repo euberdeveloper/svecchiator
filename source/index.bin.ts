451d347cc3ffa2a5605cdff8a889af2b7fa69105
euberdeveloper <euberdeveloper@gmail.com>
2022-04-21 16:19:32 +0200

    feat(source): add cli with yargs
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

---
+++ source/index.bin.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+import * as yargs from 'yargs';
+import { svecchia, DEFAULT_OPTIONS } from './index';
+
+async function main() {
+    const args = await yargs
+        .scriptName('svecchia')
+        .options({
+            'source': {
+                alias: 's',
+                describe: 'The path containing the package.json file',
+                type: 'string',
+                default: process.cwd(),
+                defaultDescription: 'The current working directory'
+            },
+            'dev': {
+                alias: 'd',
+                default: DEFAULT_OPTIONS.onlyDevDeps,
+                describe: 'If set, only the dev dependencies will be upgraded. By default both dev and prod dependencies will be upgraded.',
+                type: 'boolean'
+            },
+            'prod': {
+                alias: 'p',
+                default: DEFAULT_OPTIONS.onlyProdDeps,
+                describe: 'If set, only the prod dependencies will be upgraded. By default both dev and prod dependencies will be upgraded.',
+                type: 'boolean'
+            },
+            'clean': {
+                alias: 'c',
+                default: DEFAULT_OPTIONS.cleanCache,
+                describe: 'If set, the npm cache will be cleaned before installing the dependencies.',
+                type: 'boolean',
+            }
+        })
+        .epilogue('For more information, find our manual at https://github.com/euberdeveloper/svecchiator#readme')
+        .argv;
+    
+    await svecchia({
+        path: args.source,
+        onlyDevDeps: args.dev,
+        onlyProdDeps: args.prod,
+        cleanCache: args.clean,
+    });
+}
+main();
