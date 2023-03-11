b4b10264e18d033e8419a48800c3d5e1d3927f86
euberdeveloper <euberdeveloper@gmail.com>
2023-03-11 01:17:26 +0100

    test(integration): add test for all deps
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- test/suites/integration.test.ts
+++ test/suites/integration.test.ts
@@ -120,4 +120,47 @@ describe('Test svecchia function', function () {
             expect(mockEuberlogWarning).toHaveBeenCalledTimes(1);
         });
     });
+
+    describe('Tests with package.json with all deps', function () {
+        const assetsPath = path.join(ASSETS_PATH, 'withAllDeps');
+
+        it('Should work with default options`', async function () {
+            await svecchia({ path: assetsPath });
+
+            expect(mockExecuteAsync).toHaveBeenCalledTimes(2);
+            expect(mockExecuteAsync).toHaveBeenCalledWith('npm uninstall a b c && npm install a b c', {
+                cwd: assetsPath
+            });
+            expect(mockExecuteAsync).toHaveBeenCalledWith('npm uninstall d e f && npm install -D d e f', {
+                cwd: assetsPath
+            });
+            expect(mockEuberlogWarning).not.toHaveBeenCalled();
+        });
+
+        it('Should work and without dev deps`', async function () {
+            await svecchia({
+                path: assetsPath,
+                onlyProdDeps: true
+            });
+
+            expect(mockExecuteAsync).toHaveBeenCalledTimes(1);
+            expect(mockExecuteAsync).toHaveBeenCalledWith('npm uninstall a b c && npm install a b c', {
+                cwd: assetsPath
+            });
+            expect(mockEuberlogWarning).not.toHaveBeenCalled();
+        });
+
+        it('Should work and without prod deps`', async function () {
+            await svecchia({
+                path: assetsPath,
+                onlyDevDeps: true
+            });
+
+            expect(mockExecuteAsync).toHaveBeenCalledTimes(1);
+            expect(mockExecuteAsync).toHaveBeenCalledWith('npm uninstall d e f && npm install -D d e f', {
+                cwd: assetsPath
+            });
+            expect(mockEuberlogWarning).not.toHaveBeenCalled();
+        });
+    });
 });
